edbbf7ba313b7e009baab8254c9d20831d8f9cc0
Deanonparnisha <83837196+Deanonparnisha@users.noreply.github.com>
2021-07-31 12:14:38 +0300

    Delete plagins directory

--- plagins/mod.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-@Client.on_message(filters.command("afk" , prefixes="..") & filters.me)
-async def fac(client: Client, message: Message):
-	await message.edit("<code>.afk</code>")
