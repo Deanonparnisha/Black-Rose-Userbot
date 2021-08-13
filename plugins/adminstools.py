fe2fbc9d3e8e7ee202f462c4eec1491cdc03e7ba
Deanonparnisha <83837196+Deanonparnisha@users.noreply.github.com>
2021-08-13 21:26:30 +0300

    Create adminstools.py

---
+++ plugins/adminstools.py
@@ -0,0 +1,382 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from time import sleep, perf_counter, time
+
+
+def get_arg(message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
+def get_args(message):
+    try:
+        message = message.text
+    except AttributeError:
+        pass
+    if not message:
+        return False
+    message = message.split(maxsplit=1)
+    if len(message) <= 1:
+        return []
+    message = message[1]
+    try:
+        split = shlex.split(message)
+    except ValueError:
+        return message  # Cannot split, let's assume that it's just one long message
+    return list(filter(lambda x: len(x) > 0, split))
+
+async def CheckAdmin(message: Message):
+    """Check if we are an admin."""
+    admin = "administrator"
+    creator = "creator"
+    ranks = [admin, creator]
+
+    SELF = await app.get_chat_member(
+        chat_id=message.chat.id, user_id=message.from_user.id
+    )
+
+    if SELF.status not in ranks:
+        await message.edit("__Я не админ!__")
+        await asyncio.sleep(2)
+        await message.delete()
+
+    else:
+        if SELF.status is not admin or SELF.can_restrict_members:
+            return True
+        else:
+            await message.edit("__недостаточно прав__")
+            await asyncio.sleep(2)
+            await message.delete()
+
+@Client.on_message(filters.command("kickme", prefix) & filters.me)
+async def leave(client: Client, message: Message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Выход с чата"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    m = await message.edit('**До связи...**')
+    await asyncio.sleep(2)
+    await client.leave_chat(chat_id=message.chat.id)
+@Client.on_message(filters.command("ban", prefix) & filters.me)
+async def ban_hammer(client: Client, message: Message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Запрос на бан в беседе"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    if await CheckAdmin(message) is True:
+        reply = message.reply_to_message
+        if reply:
+            user = reply.from_user["id"]
+        else:
+            user = get_arg(message)
+            if not user:
+                await message.edit("**Я должен кого то забанить?**")
+                return
+        try:
+            reply = message.reply_to_message
+            await app.kick_chat_member(message.chat.id, reply.from_user.id, int(time.time() + 31536000))
+            await message.edit(f'<b><a href="tg://user?id={reply.from_user.id}">{reply.from_user.first_name}</a> забанен!</b>')
+        except:
+            await message.edit("**Я не могу забанить этого пользователя.**")
+    else:
+        await message.edit("**Я админ?**")
+
+@Client.on_message(filters.command("unban", prefix) & filters.me)
+async def unban(client: Client, message: Message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Запрос на разбан в беседе"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    if await CheckAdmin(message) is True:
+        reply = message.reply_to_message
+        if reply:
+            user = reply.from_user["id"]
+        else:
+            user = get_arg(message)
+            if not user:
+                await message.edit("**Я должен кого то разбанить?**")
+                return
+        try:
+            get_user = await app.get_users(user)
+            await app.unban_chat_member(chat_id=message.chat.id, user_id=get_user.id)
+            await message.edit(f"**Пользователь {get_user.first_name} был разбанен.**")
+        except:
+            await message.edit("**Я не могу разбанить.**")
+    else:
+        await message.edit("**Я админ?**")
+
+mute_permission = ChatPermissions(
+    can_send_messages=False,
+    can_send_media_messages=False,
+    can_send_stickers=False,
+    can_send_animations=False,
+    can_send_games=False,
+    can_use_inline_bots=False,
+    can_add_web_page_previews=False,
+    can_send_polls=False,
+    can_change_info=False,
+    can_invite_users=True,
+    can_pin_messages=False,
+)
+
+@Client.on_message(filters.command("mute", prefix) & filters.me)
+async def mute_hammer(client: Client, message: Message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Запрос на мут"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    if await CheckAdmin(message) is True:
+        reply = message.reply_to_message
+        if reply:
+            user = reply.from_user["id"]
+        else:
+            user = get_arg(message)
+            if not user:
+                await message.edit("**Я должен кого то замутить?**")
+                return
+        try:
+            get_user = await app.get_users(user)
+            await app.restrict_chat_member(
+                chat_id=message.chat.id,
+                user_id=get_user.id,
+                permissions=mute_permission,
+            )
+            await message.edit(f"**{get_user.first_name} Был замучен.**")
+        except:
+            await message.edit("**Я не могу замутить.**")
+    else:
+        await message.edit("**Я админ?**")
+
+unmute_permissions = ChatPermissions(
+    can_send_messages=True,
+    can_send_media_messages=True,
+    can_send_stickers=True,
+    can_send_animations=True,
+    can_send_games=True,
+    can_use_inline_bots=True,
+    can_add_web_page_previews=True,
+    can_send_polls=True,
+    can_change_info=False,
+    can_invite_users=True,
+    can_pin_messages=False,
+)
+
+@Client.on_message(filters.command("unmute", prefix) & filters.me)
+async def unmute(client: Client, message: Message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Запрос на размут"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    if await CheckAdmin(message) is True:
+        reply = message.reply_to_message
+        if reply:
+            user = reply.from_user["id"]
+        else:
+            user = get_arg(message)
+            if not user:
+                await message.edit("**Я должен кого то размутить?**")
+                return
+        try:
+            get_user = await app.get_users(user)
+            await app.restrict_chat_member(
+                chat_id=message.chat.id,
+                user_id=get_user.id,
+                permissions=unmute_permissions,
+            )
+            await message.edit(f"**{get_user.first_name} Был размучен.**")
+        except:
+            await message.edit("**Я не могу размутить.**")
+    else:
+        await message.edit("**Я админ?**")
+
+@Client.on_message(filters.command("kick", prefix) & filters.me)
+async def kick_user(client: Client, message: Message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Запрос на кик участника"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    if await CheckAdmin(message) is True:
+        reply = message.reply_to_message
+        if reply:
+            user = reply.from_user["id"]
+        else:
+            user = get_arg(message)
+            if not user:
+                await message.edit("**Я должен кого то кикнуть?**")
+                return
+        try:
+            get_user = await app.get_users(user)
+            await app.kick_chat_member(
+                chat_id=message.chat.id,
+                user_id=get_user.id,
+            )
+            await message.edit(f"**Пользователь {get_user.first_name} был кикнут.**")
+        except:
+            await message.edit("**Я не могу кикать.**")
+    else:
+        await message.edit("**Я админ?**")
+
+@Client.on_message(filters.command("pin", prefix) & filters.me)
+async def pin_message(client: Client, message: Message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Запрос на закрепление сообщения"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    if message.chat.type in ["group", "supergroup"]:
+        admins = await app.get_chat_members(
+            message.chat.id, filter=ChatMemberFilters.ADMINISTRATORS
+        )
+        admin_ids = [user.user.id for user in admins]
+        me = await app.get_me()
+
+        if me.id in admin_ids:
+            if message.reply_to_message:
+                disable_notification = True
+
+                if len(message.command) >= 2 and message.command[1] in [
+                    "alert",
+                    "notify",
+                    "loud",
+                ]:
+                    disable_notification = False
+
+                await app.pin_chat_message(
+                    message.chat.id,
+                    message.reply_to_message.message_id,
+                    disable_notification=disable_notification,
+                )
+                await message.edit("`Сообщение закрепленно!`")
+            else:
+                await message.edit(
+                    "`Сделай ответ на сообщение`"
+                )
+        else:
+            await message.edit("`Недостаточно прав`")
+    else:
+        await message.edit("`Я админ?`")
+    await asyncio.sleep(3)
+    await message.delete()
+
+@Client.on_message(filters.command("unpin", prefix) & filters.me)
+async def pin(client: Client, message: Message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Сообщение закрепленно"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    try:
+        message_id = message.reply_to_message.message_id
+        await client.unpin_chat_message(message.chat.id, message_id)
+        await message.edit('<code>Открепленно! </code>')
+    except:
+        await message.edit('<b>Сделайте реплай сообщению</b>')
+
+@Client.on_message(filters.command("admin", prefix) & filters.me)
+async def promote(client, message: Message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Выдан статус админа одному из участников"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    if await CheckAdmin(message) is False:
+        await message.edit("**Я не админ.**")
+        return
+    title = "Admin"
+    reply = message.reply_to_message
+    if reply:
+        user = reply.from_user["id"]
+        title = str(get_arg(message))
+    else:
+        args = get_args(message)
+        if not args:
+            await message.edit("**Я должен кого то повысить?**")
+            return
+        user = args[0]
+        if len(args) > 1:
+            title = " ".join(args[1:])
+    get_user = await app.get_users(user)
+    try:
+        await app.promote_chat_member(message.chat.id, user, can_pin_messages=True)
+        if title == "":
+            title = "Админ"
+        await message.edit(
+            f"**{get_user.first_name} Стал админом с званием [{title}]**"
+        )
+    except Exception as e:
+        await message.edit(f"{e}")
+    if title:
+        try:
+            await app.set_administrator_title(message.chat.id, user, title)
+        except:
+            pass
+
+@Client.on_message(filters.command("unadmin", prefix) & filters.me)
+async def demote(client, message: Message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Отобран статус админа одному из участников"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    if await CheckAdmin(message) is False:
+        await message.edit("**Я не админ**")
+        return
+    reply = message.reply_to_message
+    if reply:
+        user = reply.from_user["id"]
+    else:
+        user = get_arg(message)
+        if not user:
+            await message.edit("**Я могу разжаловать админа?**")
+            return
+    get_user = await app.get_users(user)
+    try:
+        await app.promote_chat_member(
+            message.chat.id,
+            user,
+            is_anonymous=False,
+            can_change_info=False,
+            can_delete_messages=False,
+            can_edit_messages=False,
+            can_invite_users=False,
+            can_promote_members=False,
+            can_restrict_members=False,
+            can_pin_messages=False,
+            can_post_messages=False,
+        )
+        await message.edit(
+            f"**{get_user.first_name} Больше не админ!**"
+        )
+    except Exception as e:
+        await message.edit(f"{e}")
+
+@Client.on_message(filters.command("invite", prefix) & filters.me)
+async def invite(client, message):
+    now = datetime.datetime.now()
+    timnow = now.strftime("Дата %d.%m.%Y • Время %H:%M:%S")
+    log = logi + timnow + "\n╰ Участник приглашён"
+    await app.send_message("Futurama_inv_revolutions_bot", log)
+
+    reply = message.reply_to_message
+    if reply:
+        user = reply.from_user["id"]
+    else:
+        user = get_arg(message)
+        if not user:
+            await message.edit("**Я должен кого то пригласить?**")
+            return
+    get_user = await app.get_users(user)
+    try:
+        await app.add_chat_members(message.chat.id, get_user.id)
+        await message.edit(f"**Пользователь {get_user.first_name} Был приглашён в этот чат!**")
+    except Exception as e:
+        await message.edit(f"{e}")
