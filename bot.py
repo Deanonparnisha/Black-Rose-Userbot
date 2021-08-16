14da42caec63066bcb62881545fb65ccd0bd7c07
Deanonparnisha <83837196+Deanonparnisha@users.noreply.github.com>
2021-08-16 04:19:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1952,4 +1952,21 @@ async def pidorasya(client: Client, message: Message):
     await asyncio.sleep(1)
     await message.edit("Не топ")
 
+@app.on_message(filters.command(['google', 'g'], prefix) & filters.me)
+async def webshot(client: Client, message: Message):
+    user_request = ' '.join(message.command[1:])
+
+    if user_request == '':
+        if message.reply_to_message:
+            reply_user_request = message.reply_to_message.text
+            request = reply_user_request.replace(' ', '+')
+            full_request = f'https://lmgtfy.app/?s=g&iie=1&q={request}'
+            await message.edit(f'<a href={full_request}>{reply_user_request}</a>', disable_web_page_preview=True)
+
+    else:
+        request = user_request.replace(' ', '+')
+        full_request = f'https://lmgtfy.app/?s=g&iie=1&q={request}'
+        await message.edit(f'<a href={full_request}>{user_request}</a>', disable_web_page_preview=True)
+
+
 app.run()
